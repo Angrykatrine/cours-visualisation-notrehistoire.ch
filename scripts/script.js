aa48e513e44a2dfcc77a77818a3f6d05e09c4d58
Mathias Winiger <63304731+Angrykatrine@users.noreply.github.com>
2024-06-09 17:43:05 +0200

    Add checkbox system

--- scripts/script.js
+++ scripts/script.js
@@ -33,42 +33,77 @@ function ready(error, dataGeo, data) {
         .attr("stroke", "white")
         .attr("stroke-width", 1);
 
+    // Create color scale
+    var color = d3.scaleOrdinal()
+      .domain(["A", "B"])
+      .range([ "Teal", "MidnightBlue"])
+
     // Create and visualise geolocated points
     svg
         .selectAll("points")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", function (d) {
-            return projection([+d.long, +d.lat])[0];
-        })
-        .attr("cy", function (d) {
-            return projection([+d.long, +d.lat])[1];
-        })
-        .attr("r", 6)
-        .style("fill", "midnightblue")
-        .attr("fill-opacity", 0.05);
+            .attr("class", function(d){ 
+                return d.featured;
+            })
+            .attr("cx", function (d) {
+                return projection([+d.long, +d.lat])[0];
+            })
+            .attr("cy", function (d) {
+                return projection([+d.long, +d.lat])[1];
+            })
+            .attr("r", 6)
+            .style("fill", function(d){
+                return color(d.featured);
+            })
+            .attr("fill-opacity", 0.05);
 
-  // Add title and legend
+    // Add title and legend
     svg
         .append("text")
-        .attr("text-anchor", "start")
-        .style("fill", "black")
-        .attr("x", 60)
-        .attr("y", 60)
-        .attr("width", 90)
-        .html("notreHistoire.ch – géolocalisation des publications")
-        .style("font-family", "sans-serif")
-        .style("font-size", 16)
+            .attr("text-anchor", "start")
+            .style("fill", "black")
+            .attr("x", 60)
+            .attr("y", 60)
+            .attr("width", 90)
+            .html("notreHistoire.ch – géolocalisation des publications")
+            .style("font-family", "sans-serif")
+            .style("font-size", 20)
 
     svg
         .append("text")
-        .attr("text-anchor", "start")
-        .style("fill", "black")
-        .attr("x", 60)
-        .attr("y", 80)
-        .attr("width", 90)
-        .html("30'114 entrées, état au 30 avril 2024")
-        .style("font-family", "monospace")
-        .style("font-size", 11)
+            .attr("text-anchor", "start")
+            .style("fill", "black")
+            .attr("x", 60)
+            .attr("y", 80)
+            .attr("width", 90)
+            .html("30'114 entrées, état au 30 avril 2024")
+            .style("font-family", "monospace")
+            .style("font-size", 11)
+
+    // Show and hide points based on checkbox selection
+    function update(){
+
+        // Check value of checkbox
+        d3.selectAll(".checkbox").each(function(d){
+          checkbox = d3.select(this);
+          group = checkbox.property("value")
+  
+          // If the box is check, show the group
+          if(checkbox.property("checked")){
+            svg.selectAll("."+group).transition().duration(1000).style("opacity", 1).attr("r", 6)
+  
+          // Otherwise hide it
+          }else{
+            svg.selectAll("."+group).transition().duration(1000).style("opacity", 0).attr("r", 0)
+          }
+        })
+      }
+  
+      // When a button change, run the update function
+      d3.selectAll(".checkbox").on("change",update);
+  
+      // Initialize at start
+      update()
 }
