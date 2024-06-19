2525e8d40c571ef19d1bc8d9de9d0795dcffe0b3
Mathias Winiger <63304731+Angrykatrine@users.noreply.github.com>
2024-06-19 11:54:20 +0200

    Update Zoom script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function ready(error, data) {
     L.tileLayer(
         'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
             attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-            maxZoom: 16,
-            minZoom: 3,
+            maxZoom: 17,
+            minZoom: 4,
         }
     ).addTo(map);
 
